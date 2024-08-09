aea8eb877fec6d7e1575b38b0a2a98964c362123
AhmedEmad226 <ahmedemad2262008@gmail.com>
2024-08-09 15:32:22 +0300

    added sum

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     int x,y;
     cin >> x >> y;
-    cout << endl << "x*y = " << endl;
+    cout << endl << "x * y = " << x*y << endl;
+    cout << endl << "x + y = " << x+y << endl;
 }
