0f40522a28ba878c4c317eb3ba2bbd5853b5a1ab
AhmedEmad226 <ahmedemad2262008@gmail.com>
2024-08-10 14:10:55 +0300

    i made a change
    this is a change

--- main.cpp
+++ main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
-
+#include <string>
+#include <exception>
 using namespace std;
 
+class Circle{
+    double radius = 1;
+    string color = "red";
+
+public:
+    Circle(){
+        radius = 0;
+    }
+
+    Circle(double r){
+        radius = r;
+    }
+
+    Circle(double r, string color){
+        radius = r;
+        this->color = color;
+    }
+
+    double getRadius(){
+        return radius;
+    }
+
+    string getColor(){
+        return color;
+    }
+
+    void setRadius(double r){
+        radius = r;
+    }
+
+    void setColor(string c){
+        color = c;
+    }
+
+    double getArea(){
+     return 3.14 * radius*radius;
+    }
+};
+
 int main() {
-    int x,y;
-    cin >> x >> y;
-    cout << endl << "x * y = " << x*y << endl;
-    cout << endl << "x + y = " << x+y << endl;
+    Circle c(7,"Green");
+    cout << c.getColor() << endl;
+    cout << c.getArea() << endl;
 }
